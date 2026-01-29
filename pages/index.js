1c3283180e5c1b7bf80c82585929b78c4376e84e
bonniele-dot <bonnie.le@azdag.com>
2026-01-29 01:49:02 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -34,7 +34,7 @@ export default function Home()
       <div className="flex flex-row items-center justify-evenly w-full h-96 bg-pink-50">
         <CommentedImage src='/photo1.JPG' text="this is my handsome man"/>
         <CommentedImage src='/photo2.jpg' text="and today is his birthday"/>
-        <CommentedImage src='/photo3.PNG' text="showing you my love and care"/>
+        <CommentedImage src='/photo3.PNG' text="showing you my love and care in here, hope you can feel it"/>
       </div>
 
       <div className="text-center w-full h-64 bg-pink-200 drop-shadow-xl">
@@ -51,27 +51,27 @@ export default function Home()
       </div>
 
       <div className="text-center w-full h-64 bg-pink-200 drop-shadow-xl">
-        <h1 className="p-20 tracking-wide text-6xl font-bold text-pink-400 uppercase drop-shadow-xl">So...</h1>
+        <h1 className="p-20 tracking-wide text-6xl font-bold text-pink-400 uppercase drop-shadow-xl">To the man who makes my world brighter, I might not say this enough......</h1>
       </div>
 
       <CornyQuestion 
-        question="Would you be the Elephant&apos;s Foot to my Chernobyl Disaster?"
-        nonactive="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Nuclear_symbol.svg/1024px-Nuclear_symbol.svg.png"
-        active="https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/76629/nuclear-explosion-clipart-xl.png"
+        question="I see everything you do for us, every time you explore the world with me, every time you make me laugh when Im sad, every time you believe in me, every good food we try together, every thing you teach me, every everything.."
+        nonactive="photo4.JPG"
+        active="photo5.jpg"
         primary
         left
       />
 
       <div className="flex flex-row justify-around items-center text-center w-full h-96 bg-pink-200 p-5">
-        <h1 className='w-2/3 text-pink-400 font-bold text-6xl'>Would you</h1>
+        <h1 className='w-2/3 text-pink-400 font-bold text-6xl'>You are my biggest supporter And my favorite person on earth to talk to</h1>
       </div>
 
       <div className="flex flex-row justify-around items-center text-center w-full h-96 bg-pink-50 p-5">
-        <h1 className='w-2/3 text-pink-300 font-bold text-6xl'>Uh</h1>
+        <h1 className='w-2/3 text-pink-300 font-bold text-6xl'>Thank you for working so hard for our future thank you for choosing me every day.</h1>
       </div>
 
       <CornyQuestion 
-        question="Would you be the butt to my Red Guy?"
+        question="I see how hard you work and I appreciate everything you do"
         nonactive="https://loudbooru.com/_images/a1c2fa54153d30da1045a967fe8fd8a9/9228%20-%20character%3Alincoln_loud%20character%3Athe_red_guy%20cow_and_chicken%20crossover%20devil%20dialogue%20satan.png"
         active="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/860bb581-65ee-4112-854f-1255c5579143/dffb3t8-d1a0991f-db5c-49ff-a7f2-2bc8d3278004.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzg2MGJiNTgxLTY1ZWUtNDExMi04NTRmLTEyNTVjNTU3OTE0M1wvZGZmYjN0OC1kMWEwOTkxZi1kYjVjLTQ5ZmYtYTdmMi0yYmM4ZDMyNzgwMDQuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.F2pBVw7v1HfA3zLlC5V5lc1xberJTFDwLT_GoKlT7Vw"
         secondary
@@ -79,7 +79,7 @@ export default function Home()
       />
 
       <CornyQuestion 
-        question="Would you be the smut to my Among Us?"
+        question="Today is not just a celebration of your new age - It’salso  a celebration of the amazing man you have become"
         nonactive="https://assets.stickpng.com/images/61d183263a856e0004c6334a.png"
         active="https://pbs.twimg.com/media/E2ieswGVgAYKWt3.jpg"
         primary
@@ -87,7 +87,23 @@ export default function Home()
       />
 
       <CornyQuestion 
-        question="Would you be the banana to my Minion?"
+        question="I am so proud to call you mine & I love you more than words can ever say"
+        nonactive="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/0a76b087-d923-4acf-85c5-118e6b81c7a0/d70lfsz-0a567aaf-a3ec-4cca-ac87-3820e4727e8f.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzBhNzZiMDg3LWQ5MjMtNGFjZi04NWM1LTExOGU2YjgxYzdhMFwvZDcwbGZzei0wYTU2N2FhZi1hM2VjLTRjY2EtYWM4Ny0zODIwZTQ3MjdlOGYucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.PZQsxdrOyzYABwsLATW4K94fX8RQBzUIzLrR-l8BDPU"
+        active="https://i.redd.it/v0ep5v1vbl331.png"
+        secondary
+        right
+       />
+
+      <CornyQuestion 
+        question="Wish you health and wealth ~ May all your hard work be rewarded, hope my man can achieve everything he wants in life"
+        nonactive="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/0a76b087-d923-4acf-85c5-118e6b81c7a0/d70lfsz-0a567aaf-a3ec-4cca-ac87-3820e4727e8f.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzBhNzZiMDg3LWQ5MjMtNGFjZi04NWM1LTExOGU2YjgxYzdhMFwvZDcwbGZzei0wYTU2N2FhZi1hM2VjLTRjY2EtYWM4Ny0zODIwZTQ3MjdlOGYucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.PZQsxdrOyzYABwsLATW4K94fX8RQBzUIzLrR-l8BDPU"
+        active="https://i.redd.it/v0ep5v1vbl331.png"
+        primary
+        left
+       />
+
+      <CornyQuestion 
+        question="And may I always be there to cheer you on."
         nonactive="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/0a76b087-d923-4acf-85c5-118e6b81c7a0/d70lfsz-0a567aaf-a3ec-4cca-ac87-3820e4727e8f.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzBhNzZiMDg3LWQ5MjMtNGFjZi04NWM1LTExOGU2YjgxYzdhMFwvZDcwbGZzei0wYTU2N2FhZi1hM2VjLTRjY2EtYWM4Ny0zODIwZTQ3MjdlOGYucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.PZQsxdrOyzYABwsLATW4K94fX8RQBzUIzLrR-l8BDPU"
         active="https://i.redd.it/v0ep5v1vbl331.png"
         secondary
@@ -95,7 +111,7 @@ export default function Home()
       />
 
       <div className="flex flex-row justify-around items-center text-center w-full h-96 bg-pink-50 px-5 pt-5">
-        <h1 className='w-2/3 text-pink-400 font-bold text-6xl'>If I&apos;m C would you be my C++ to prom?</h1>
+        <h1 className='w-2/3 text-pink-400 font-bold text-6xl'>Are you ready for another year growing old with me???</h1>
       </div>
 
       <div className="flex flex-row justify-around items-start text-center w-full h-36 bg-pink-50 pb-5">
@@ -125,7 +141,7 @@ export default function Home()
               </AlertDialogHeader>
 
               <AlertDialogBody>
-                NO WAY SHE JUST SAID YES?
+                You promise?
               </AlertDialogBody>
 
               <AlertDialogFooter>
