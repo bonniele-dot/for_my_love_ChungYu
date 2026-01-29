b9fd35875ae3a4d4cb8f8e7b1c8d67c65bb698f7
bonniele-dot <bonnie.le@azdag.com>
2026-01-29 01:08:51 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -38,16 +38,16 @@ export default function Home()
       </div>
 
       <div className="text-center w-full h-64 bg-pink-200 drop-shadow-xl">
-        <h1 className="p-20 tracking-wide text-6xl font-bold text-pink-400 uppercase drop-shadow-xl">So I heard...</h1>
+        <h1 className="p-20 tracking-wide text-6xl font-bold text-pink-400 uppercase drop-shadow-xl">Today is a very special day...</h1>
       </div>
 
       <div className="flex flex-col items-center text-center w-full h-96 bg-pink-50 p-10">
         <div className="flex flex-row items-center text-center w-full h-5/6 bg-pink-50">
-          <h1 className='w-1/3 text-pink-300 font-bold text-6xl'>You Need</h1>
+          <h1 className='w-1/3 text-pink-300 font-bold text-6xl'>Because on this exact day 29 years ago</h1>
           <img src='https://sweezy-cursors.com/wp-content/uploads/cursor/milk-amp-mocha-bear-love/milk-mocha-bear-love-custom-cursor.png' width={600}/>
-          <h1 className='w-1/3 text-pink-300 font-bold text-6xl'>Someone For</h1>
+          <h1 className='w-1/3 text-pink-300 font-bold text-6xl'>my favorite person in the world was born.</h1>
         </div>
-        <h1 className="w-full h-1/4 text-pink-300 font-bold text-6xl uppercase">prom</h1>
+        <h1 className="w-full h-1/4 text-pink-300 font-bold text-6xl uppercase">and Im so happy that I met him in this life</h1>
       </div>
 
       <div className="text-center w-full h-64 bg-pink-200 drop-shadow-xl">
